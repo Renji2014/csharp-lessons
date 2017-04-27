83012a7845097767d72799bf318b39a6f3437603
Evilham <github@evilham.com>
2017-04-28 00:41:35 +0200

    Added a summary of 1st lesson.

---
+++ latex/slides/resources/01_grundlagen_1/zusammenfassung.cs
@@ -0,0 +1,22 @@
+using System; // Namespace import
+namespace HelloWorld
+{
+    class Hello
+    {
+        public static void Main()
+        {
+            double x;
+            int p = 3;
+            x = do_stuff(p); // Impl. Cast int --> double
+            p = (int)x; // Expl. Cast double 
+                        //                 --> _nearest_ int
+            Console.WriteLine(string.Format(
+                    "p is now 4: {0}, x is 4: {1}", p, x));
+            Console.ReadKey(); // Sonst ist die Konsole weg!
+        }
+        public static int do_stuff(int p)
+        {
+            return ((p * 3 - 2) + 2) / 2;
+        }
+    }
+}
